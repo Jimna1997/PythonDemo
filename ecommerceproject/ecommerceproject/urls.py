413b0adacd806a6ff2be4c806d026150faf7b3f1
unknown <jimnakurikkal2gmail.com>
2022-12-06 15:14:41 +0530

    Added

---
+++ ecommerceproject/ecommerceproject/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from ecommerceproject import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('shop/', include('ecommerceapp.urls')),
+    path('search/', include('search_app.urls')),
+    path('cart/', include('cart_app.urls'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
