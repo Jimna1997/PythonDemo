0152cb375b96a1aef337c6e40374a5937f705e30
Jimna1997 <119839750+Jimna1997@users.noreply.github.com>
2022-12-16 19:49:41 +0530

    Delete movie directory

--- movie/movieapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from movieapp.models import Movie
-
-class MovieForm(forms.ModelForm):
-    class Meta:
-        # model to be edited
-        model=Movie
-        # fields to edit
-        fields=['name','desc','img','year']
