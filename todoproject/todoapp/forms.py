cca18b7ba338874c067f8220e0fbb7c7c3c26b23
Jimna1997 <119839750+Jimna1997@users.noreply.github.com>
2022-12-16 19:50:27 +0530

    Delete todoproject directory

--- todoproject/todoapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from todoapp.models import Task
-
-class TodoForm(forms.ModelForm):
-    class Meta:
-        # model to be edited
-        model=Task
-        # fields to edit
-        fields=['name','priority','date']
