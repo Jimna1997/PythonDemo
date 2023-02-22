59438d56523a915d48800cdf5ba21b8293d6aaae
Jimna1997 <119839750+Jimna1997@users.noreply.github.com>
2023-02-22 15:20:44 +0530

    Delete SchoolStore directory

--- SchoolStore/Schoolapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Department,Student
-
-# Register your models here.
-class deptAdmin(admin.ModelAdmin):
-    list_display = ['name']
-admin.site.register(Department,deptAdmin)
-
-admin.site.register(Student)
